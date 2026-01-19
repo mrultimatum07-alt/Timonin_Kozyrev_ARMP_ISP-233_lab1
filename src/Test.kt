fc886da62541a06d47ad85755e617847d60d8ae0
mrultimatum07-alt <mrultimatum07@gmail.com>
2026-01-19 14:34:15 +0300

    Add abstract class

--- src/Test.kt
+++ src/Test.kt
@@ -1,14 +1,30 @@
-var age: Int = 18
-    set(value) {
-        if ((value > 0) and (value < 110))
-            field = value
+//var age: Int = 18
+//    set(value) {
+//        if ((value > 0) and (value < 110))
+//            field = value
+//    }
+//data class Item(
+//        val id: Int,
+//        val name: String,
+//        val quantity: Int
+//        )
+
+abstract  class Human(val name: String){
+    abstract var age: Int
+    abstract fun hello()
+
+}
+class Person(name: String, override var age: Int): Human(name){
+    override fun hello(){
+        println("My name is $name")
     }
-data class Item(
-        val id: Int,
-        val name: String,
-        val quantity: Int
-        )
+}
 fun main() {
+    val denis: Person = Person("Denis",1)
+    val maksim: Person = Person("Maksim",1000)
+    denis.hello()
+    maksim.hello()
+//    val pavel: Human = Human("Pavel")
 //    println(age)
 //    age = 45
 //    println(age)
